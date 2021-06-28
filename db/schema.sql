3aae2d6b4af2c050dc0dd21b7dd05b5b41d00507
Nick Johnson <nicklynnjohnson@gmail.com>
2021-06-27 21:55:30 -0700

    Finished formatting on the view. Initial work on JS public file.

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ USE eat_da_burger_db;
 -- -- Department table -- -- 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(30) NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
