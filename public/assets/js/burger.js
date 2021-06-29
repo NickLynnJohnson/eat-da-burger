c6f61d1046f1e1d3a0bf8b0a80514f3cd20624b7
Nick Johnson <nicklynnjohnson@gmail.com>
2021-06-28 21:55:13 -0700

    Able to console.log form value via submit button.

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,14 +1,14 @@
 
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(document).ready(function(){
-  $(".input.autocomplete").autocomplete({
+$(document).ready(function() {
+  $(".submit-burger").on("click", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newBurger = {
-      burger_name: $("#newburger").val().trim(),
+      burger_name: $("input#typed-burger").val().trim(),
       devoured: false
-    };
+    }; 
 
     // Send the POST request.
     $.ajax("/api/burgers", {
@@ -16,35 +16,52 @@ $(document).ready(function(){
       data: newBurger
     }).then(
       function() {
-        console.log("created new burger");
+        console.log("created new burger" + newBurger);
         // Reload the page to get the updated list
         location.reload();
       }
     );
+
   });
-
-  $(".change-sleep").on("click", function(event) {
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
-
-    var newSleepState = {
-      sleepy: newSleep
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
-      type: "PUT",
-      data: newSleepState
-    }).then(
-      function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  
-
-
 });
+
+//   // Replace all alerts with modals
+
+//   const userData = {
+//     username: usernameInput.val().trim(),
+//     email: emailInput.val().trim(),
+//     password: passwordInput.val().trim()
+//   };
+
+//   if (!userData.username || !userData.email || !userData.password) {
+//     return alert("Please don't leave fields blank");
+//   }
+
+//   // If we have an email and password, run the signUpUser function
+//   signUpUser(userData.username, userData.email, userData.password);
+//   emailInput.val("");
+//   passwordInput.val("");
+//   usernameInput.val("");
+//   repeatPasswordInput.val("");
+//   repeatEmailInput.val("");
+// });
+
+// // Does a post to the signup route. If succesful, we are redirected to the members page
+// // Otherwise we log any errors
+// function signUpUser(username, email, password) {
+//   $.post("/users/signup", {
+//     username: username,
+//     email: email,
+//     password: password
+//   }).then(function(data) {
+//     if (data.duplicateUser) {
+//       // Replace with Modal
+//       alert("Sorry, that username has been taken");
+//     } else {
+//       window.location = data.redirect;
+//     }
+//   }).catch(function(err) {
+//     console.log(err);
+//   });
+// }
+
